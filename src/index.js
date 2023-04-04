34598d90131b4e39e85a54d25f3d26a94475c848
stuwilmur <stuwilmur@users.noreply.github.com>
2023-04-04 22:54:29 +0100

    Change var name

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import {autoType, csvParse} from 'd3';
-import {sData} from './data/data';
-export const data = csvParse(sData, autoType);
+import {csv} from './data/data';
+export const data = csvParse(csv, autoType);
