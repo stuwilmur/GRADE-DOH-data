bdcb6f49a790ecffe91aba2ecf8de680a6295114
stuwilmur <stuwilmur@users.noreply.github.com>
2023-04-04 21:27:51 +0100

    Replace .csv with data.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 import {autoType, csvParse} from 'd3';
-import {readFileSync} from 'fs';
-
-const dataString = readFileSync('./data/BASE data 2022 interpolated.csv', {
-  encoding: 'utf8',
-  flag: 'r',
-});
-export const data = csvParse(dataString, autoType);
+import {sData} from './data/data';
+export const data = csvParse(sData, autoType);
